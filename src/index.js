3665678f05a8dfd6bbdc3012f5206c0b725dcc9f
phaivanh <94913115+phaivanh@users.noreply.github.com>
2021-11-23 21:52:39 +0100

    comment code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+// spared operator on array
 const Tab = [10, 20, 30];
 const Tab2 = [1, 2, 3, ...Tab];
 console.log(Tab2);
 
+// spared operator on Object
 const Obj1 = {
   Name: "John",
   Age: 25

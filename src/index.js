90f9d39df8c056f7a52799fedabe1d1a488b6c1a
phaivanh <94913115+phaivanh@users.noreply.github.com>
2021-11-24 10:40:07 +0100

    Fixed issue

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-// spared operator on array
+// SPREAD OPERATOR EXTRACT ALL ELEMENT/PROPERTIES FROM ARRAY/OBJECT
+
+// spread operator on array
 const Tab = [10, 20, 30];
 const Tab2 = [1, 2, 3, ...Tab];
 console.log(Tab2);
 
-// spared operator on Object
+// spread operator on Object
 const Obj1 = {
   Name: "John",
   Age: 25
 };
+console.log("SPREAD OPERATOR");
 console.log(Obj1);
 
 const Obj2 = {
@@ -16,8 +19,9 @@ const Obj2 = {
 };
 console.log(Obj2);
 
+// REST OPERATOR USED IN FUNCTION AS PARAM AND TRANSFORM THEM IN A ARRAY
 // REST operator
-
+console.log("REST OPERATOR");
 const filter = (...params) => {
   return params.filter((e, i) => checkLimit(e, i));
 };
@@ -34,3 +38,18 @@ const filter2 = (...params) => {
 };
 console.log("filter2 ");
 console.log(filter2(5, 18, 31));
+
+// EXTRAIT QUELQUES ELEMENTS/PROPRIETES D'UN TABLEAU/OBJECT
+console.log("DESTRUCTURING");
+
+const t1 = [1, 2, 5];
+var [elt1, , elt2] = t1;
+console.log(elt1);
+console.log(elt2);
+
+const Obj3 = {
+  Name: "John",
+  Age: 25
+};
+var { Age } = Obj3;
+console.log(Age);
